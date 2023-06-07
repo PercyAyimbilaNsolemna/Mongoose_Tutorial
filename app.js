41c13ca00877d7463c5806fddbd7be4803b31a1c
master <pnayimbila@st.ug.edu.gh>
2023-06-07 14:43:46 +0000

    Add a function to add person to people collection in the fruitsDB.

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ const mongoose = require("mongoose");
 //Creates a main method 
 async function main() {
     //Creates a variable that store the URL to the mongoDB atlas cluster and connects to the fruitsDB database
-    const URL = "mongodb+srv://<username>:<password>@cluster0.az84zbp.mongodb.net/fruitsDB?retryWrites=true&w=majority";
+    
 
+    const URL = "mongodb+srv://<username>:<password>@cluster0.az84zbp.mongodb.net/fruitsDB?retryWrites=true&w=majority";
     //Uses mongoose to connect to the fruits database
     await mongoose.connect(URL);
     console.log("Connected successfully to mongoDB");
 
+    /*
     //Calls the addDocument function
     await addDocument();
+    */
+
+    //Calls the addPerson function
+    await addPerson();
 
 }
 
@@ -40,6 +46,31 @@ async function addDocument(){
 
     //Saves the document(s) added to the fruits collection or model
     fruit.save();
+}
+
+//Creates a function that inserts a Person colllection to the database  and inserts a person to it
+async function addPerson(){
+    //Creates a schema or structure for the people collection
+    const personSchema = new mongoose.Schema(
+        {
+            name: String,
+            age: Number
+        }
+    );
+
+    //Creates a model or collection for the document
+    const Person = mongoose.model("Person", personSchema);
+
+    //Adds a new person(document) to the people collection
+    const person = new Person(
+        {
+            name: "John", 
+            age: 37
+        }
+    );
+    
+    //Calls the mongoose save method to save the new person added to the people collection
+    person.save();
 }
 
 //Calls the main function and catches any error and logs it to the console
